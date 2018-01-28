166f533f7ae7fa493c8a33848ab74eb020356469
Jonas Henie <jonashenie@J.com>
2018-01-28 15:07:09 +0200

    Removed default selected ou

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,41 +1,33 @@
 import React from 'react';
 import OrgUnitTree from 'd2-ui/lib/org-unit-tree/OrgUnitTree.component';
 
-class Sidebar extends React.Component {
-  render() {
-    const { root, ouPath, onSelectClick } = this.props;
-
-    if (!root) return null;
-    return (
+const Sidebar = ({ root, ouPath, onSelectClick }) => {
+  if (!root) return null;
+  return (
+    <div
+      style={{
+        backgroundColor: '#f3f3f3',
+        borderRight: '1px solid #e1e1e1'
+      }}
+    >
       <div
         style={{
-          backgroundColor: '#f3f3f3',
-          borderRight: '1px solid #e1e1e1'
+          backgroundColor: 'white',
+          marginTop: 50,
+          height: 600,
+          width: 250,
+          overflow: 'auto'
         }}
       >
-        <div
-          style={{
-            backgroundColor: 'white',
-            marginTop: 50,
-            height: 600,
-            width: 250,
-            overflow: 'auto'
-          }}
-        >
-          <OrgUnitTree
-            root={root}
-            hideCheckboxes
-            onSelectClick={onSelectClick}
-            selected={ouPath}
-            // TODO: rmeove this property after development
-            initiallyExpanded={[
-              '/Hjw70Lodtf2/psfB4ksRKp2/DG8h5ijGxgO/sFGfRP4wPqe/oiAbfOiho08/hV87OCHgO4v'
-            ]}
-          />
-        </div>
+        <OrgUnitTree
+          root={root}
+          hideCheckboxes
+          onSelectClick={onSelectClick}
+          selected={ouPath}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Sidebar;
